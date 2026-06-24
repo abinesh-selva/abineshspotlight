5b49812849b1d120e0a87e9fe1ce29fd6c0472dd
Abinesh Selvarasu <mailtoabineshselva@gmail.com>
2026-06-24 12:16:34 +0530

    googletagmanager added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Syne, Outfit } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/next'
+import Script from 'next/script'
 import WhatsAppFloat from '@/components/WhatsAppFloat'
 import './globals.css'
 
@@ -73,6 +74,16 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         {children}
         <WhatsAppFloat />
         <Analytics />
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-P18SXZK5DY" strategy="afterInteractive" />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-P18SXZK5DY');
+          `}
+        </Script>
       </body>
     </html>
   )
